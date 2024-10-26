e424098ac4c10a8813f9b7a8abd082de89ce2fbe
GGLVXD <gglvmax@gmail.com>
2024-10-26 16:00:18 +0300

    removed React.js bloatware

---
+++ src/root/root.jsx
@@ -0,0 +1,14 @@
+// /src/root/root.jsx
+import React from 'react';
+import './root.css';
+
+function Root() {
+  return (
+    <div className="root">
+      <h1>prevolt</h1>
+      <p>rewrite soon</p>
+    </div>
+  );
+}
+
+export default Root;
